3f6858ff310e1444e9c28668f402df9b8389e746
Chien Nguyen <chiennguyen1702@gmail.com>
2025-07-08 07:05:41 +0700

    top

--- rinku/sp/summer-fashion-passport/_assets/js/top.js
+++ rinku/sp/summer-fashion-passport/_assets/js/top.js
@@ -1,18 +1,48 @@
-console.log('top.js');
+document.addEventListener("DOMContentLoaded", function() {
+  const swiper = new Swiper('.js-swiper-01', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 11,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
 
-const swiper = new Swiper('.js-swiper-01', {
-  loop: true,
-  slidesPerView: 'auto',
-  spaceBetween: 11,
+document.addEventListener("DOMContentLoaded", function() {
+  const modal = document.getElementById("popupModal");
+  const modalLogo = modal.querySelector(".modal-logo img");
+  const modalImage = modal.querySelector(".modal-image img");
+  const closeBtn = modal.querySelector(".close-btn");
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  document.querySelectorAll(".swiper-slide").forEach(slide => {
+    slide.addEventListener("click", () => {
+      const logoImg = slide.querySelector(".logo img").getAttribute("src");
+      const mainImg = slide.querySelector(".image img").getAttribute("src");
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+      modalLogo.setAttribute("src", logoImg);
+      modalImage.setAttribute("src", mainImg);
+
+      modal.style.display = "block";
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  // Đóng khi click ra ngoài
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 });
